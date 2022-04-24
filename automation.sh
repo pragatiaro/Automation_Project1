4d9a5a352d629bb5fc0534b1ff373bc61a3d30fb
pragatiaro <pragatigoraya@gmail.com>
2022-04-24 18:03:37 +0000

    Task3 Commit

--- automation.sh
+++ automation.sh
@@ -41,3 +41,26 @@ cd /var/log/apache2/
 tar -cvf /tmp/${myname}-httpd-logs-${timestamp}.tar *.log
 size=$(sudo du -sh /tmp/${myname}-httpd-logs-${timestamp}.tar | awk '{print $1}')#!/bin/bash
 #End of Task 2
+
+#Task 3 - To keep logs in inventory.html
+	if [ -e /var/www/html/inventory.html ]
+	then
+	echo "<br>httpd-logs &nbsp;&nbsp;&nbsp; ${timestamp} &nbsp;&nbsp;&nbsp; tar &nbsp;&nbsp;&nbsp; ${size}" >> /var/www/html/inventory.html
+	else
+	echo "<b>Log Type &nbsp;&nbsp;&nbsp;&nbsp; Date Created &nbsp;&nbsp;&nbsp;&nbsp; Type &nbsp;&nbsp;&nbsp;&nbsp; Size</b><br>" > /var/www/html/inventory.html
+	echo "<br>httpd-logs &nbsp;&nbsp;&nbsp; ${timestamp} &nbsp;&nbsp;&nbsp; tar &nbsp;&nbsp;&nbsp; ${size}" >> /var/www/html/inventory.html
+	fi
+
+
+#Pushing to AWS S3 bucket
+aws s3 \
+cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
+s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar 
+
+# check cron file is exist of not, if it is doesn't exist then create it 
+# Note:- script will execute once in day at 3.30AM 
+if  [ ! -f  /etc/cron.d/automation ]
+then
+	echo "30 3 * * * root /root/Automation_project1/automation.sh" > /etc/cron.d/automation
+fi
+
